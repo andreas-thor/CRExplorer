a8437a45ce3c88e23968e599d95e6d2e70fc22fc
Andreas Thor <andreas.thor@htwk-leipzig.de>
2022-07-20 15:01:25 +0200

    webseite aktualisiert

---
+++ app/src/main/java/cre/Script.java
@@ -0,0 +1,14 @@
+package cre;
+
+import java.io.IOException;
+
+import org.codehaus.groovy.control.CompilationFailedException;
+
+import cre.scriptlang.ScriptExecutionEngine;
+
+public class Script {
+    
+    public static void main(String[] args) throws CompilationFailedException, IOException {
+        ScriptExecutionEngine.main(args);
+    }
+}
